c88189ccc38802821e8732576f2586807b8dca28
bashartech <144168218+bashartech@users.noreply.github.com>
2024-11-20 23:56:27 +0500

    update

--- components/Section1.tsx
+++ components/Section1.tsx
@@ -7,7 +7,7 @@ export default function Section1() {
       <div className="sec1 bg-[#F7F7F7]   h-[228px] w-screen md:flex justify-around items-center">
         <div className="left md:p-0 px-5 flex justify-center items-center w-[428px] md:w-[320px] md:h-[68px] h-[120px]">
             <h1 className='text-[20px] lg:pl-0 pl-3 lg:text-[24px] md:flex hidden font-bold'>Trusted by 2000+ companies  worldwide.</h1>
-            <h1 className='ml-5 text-[18px] md:hidden font-bold'>Trusted by the world's best companies [social proof to build credibility]</h1>
+            <h1 className='ml-5 text-[18px] md:hidden font-bold'>Trusted by the worlds best companies [social proof to build credibility]</h1>
         </div>
 <div className="right overflow-hidden flex md:pl-0 pl-8  md:justify-end justify-start gap-5 md:w-[880px] md:h-[56px] h-[70px]  ">
 <Image className='object-fit w-[123px] h-full' src={"images/tag1.svg"} width={123} height={38.52} alt=''></Image>
